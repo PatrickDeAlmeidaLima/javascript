6d5a48b1f5a4c3655a89846a82ddf721b0366b93
unknown <patrick.almeida.lima@gmail.com>
2023-01-23 20:57:28 -0300

    Destructuring

--- conteudo/scripts.js
+++ conteudo/scripts.js
@@ -132,8 +132,29 @@
 
 //----------------------------------------------------------------------------------------------------------
 
-// 5 - templates literals
-const userName = "Patrick";
-const age = 30;
+// // 5 - templates literals
+// const userName = "Patrick";
+// const age = 30;
 
-console.log(`O nome do usuário é: ${userName} e ele tem:${age} anos.`);
+// console.log(`O nome do usuário é: ${userName} e ele tem:${age} anos.`);
+//----------------------------------------------------------------------------------------------------------
+
+// 6 - Destructuring
+
+const fruits = ["Maçã", "Banana", "Mamão"]
+
+const [f1, f2, f3] = fruits
+
+console.log(f1)
+console.log(f3)
+
+const productsDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Cinza"
+}
+
+const { name: productName, price, category: productCategory, color } = productsDetails
+
+console.log(`O nome do produto é ${productName}, custa R$${price} pertence a categoria ${productCategory} e é da cor ${color}.`)
