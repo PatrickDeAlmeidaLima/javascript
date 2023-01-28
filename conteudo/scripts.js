88bb8c8c9896bf122ac7d26d359d5947e2305e0e
unknown <patrick.almeida.lima@gmail.com>
2023-01-27 21:53:39 -0300

    heranças

--- conteudo/scripts.js
+++ conteudo/scripts.js
@@ -209,3 +209,31 @@ console.log(tenis.name)
 console.log(tenis.productWithDiscount(20))
 
 console.log(tenis.productWithDiscount(60))
+
+//----------------------------------------------------------------------------------------------------------
+
+// 9 - Herança
+
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+
+    showColors() {
+        console.log("As cores são: ")
+        this.colors.forEach((color) => {
+            console.log(color)
+        })
+    }
+}
+
+
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, ["Azul", "Preto", "Rosa", "Branco", "Amarelo"])
+
+console.log(hat.name)
+
+console.log(hat.productWithDiscount(30))
+
+hat.showColors()
