c884f151d00990485fd0f24846af7fff7b9977f5
unknown <patrick.almeida.lima@gmail.com>
2023-02-02 22:26:34 -0300

    Complementando dicas

---
+++ API/2_rota_post/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+
+app.use(
+    express.urlencoded({
+        extended: true,
+
+    })
+)
+
+app.use(express.json())
+
+// rotas - endpoints
+
+app.post('/createproducts', (req, res) => {
+
+    const name = req.body.name
+    const price = req.body.price
+
+    console.log(name)
+    console.log(price)
+
+    res.json({ message: `O produto ${name} foi criado com sucesso e com valor de R$${price}` })
+
+})
+
+app.get('/', (req, res) => {
+
+    res.json({ message: "Primeira rota criada com sucesso!" })
+
+})
+
+app.listen(3000)
